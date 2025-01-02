135593428f37bfa9eef4281fd71085611cca4bea
Mark Lenser <mlenser@gmail.com>
2025-01-02 23:07:06 +0000

    fix: delay validation to conform to RPC limit

--- scripts/_/validate-gauge-names.ts
+++ scripts/_/validate-gauge-names.ts
@@ -9,6 +9,33 @@ import { berachainTestnetbArtio } from 'viem/chains'
 
 import type { GaugeListSchema } from '@/types/gauge-list'
 
+import { delay } from './delay'
+
+const RPC_REQUESTS_PER_SECOND = 10
+
+const publicClient = createPublicClient({
+  chain: berachainTestnetbArtio,
+  transport: fallback([http()]),
+})
+
+const getVaultSymbol = async ({
+  errors,
+  underlyingToken,
+}: {
+  errors: Array<string>
+  underlyingToken: string
+}) => {
+  const symbol = await publicClient.readContract({
+    abi: erc20Abi,
+    address: underlyingToken as Address,
+    functionName: 'symbol',
+  })
+  if (!symbol) {
+    errors.push(`${underlyingToken} does not have a symbol on the contract.`)
+  }
+  return symbol
+}
+
 export const validateGaugeNames = async ({
   errors,
   list,
@@ -16,40 +43,24 @@ export const validateGaugeNames = async ({
   errors: Array<string>
   list: GaugeListSchema
 }) => {
-  const publicClient = createPublicClient({
-    chain: berachainTestnetbArtio,
-    transport: fallback([
-      http(
-        `https://${process.env.QUIKNODE_SUBDOMAIN}.bera-bartio.quiknode.pro/${process.env.QUIKNODE_KEY}`,
-      ),
-      http(),
-    ]),
-  })
   const gauges: GaugeListSchema['gauges'] = list.gauges
+  let rpcLookupCount = 0
 
-  await Promise.all(
-    gauges.map(async (gauge) => {
-      const symbols = await Promise.all(
-        gauge.underlyingTokens.map(async (underlyingToken) => {
-          const symbol = await publicClient.readContract({
-            abi: erc20Abi,
-            address: underlyingToken as Address,
-            functionName: 'symbol',
-          })
-          if (!symbol) {
-            errors.push(
-              `${underlyingToken} does not have a symbol on the contract.`,
-            )
-          }
+  for (const gauge of gauges) {
+    const symbols = await Promise.all(
+      gauge.underlyingTokens.map(async (underlyingToken) => {
+        rpcLookupCount += 1
+        if (rpcLookupCount % RPC_REQUESTS_PER_SECOND === 0) {
+          console.log('delay')
+          await delay(1000)
+        }
+        return await getVaultSymbol({ errors, underlyingToken })
+      }),
+    )
+    const name = symbols.join('-')
 
-          return symbol
-        }),
-      )
-      const name = symbols.join('-')
-
-      if (gauge.name !== name) {
-        errors.push(`${gauge.name} does not match ${name}.`)
-      }
-    }),
-  )
+    if (gauge.name !== name) {
+      errors.push(`${gauge.name} does not match ${name}.`)
+    }
+  }
 }
