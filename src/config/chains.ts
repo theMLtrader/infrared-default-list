5f7ab142646ed9f33792fbd305acc83a58f7f883
Mark Lenser <mlenser@gmail.com>
2025-02-03 19:56:21 +0000

    fix: mainnet gauges, tokens, and assets

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,8 +1,10 @@
 import { berachainTestnetbArtio } from 'viem/chains'
 
 import { berachainTestnetcArtio } from './berachain-testnet-cartio'
+import { berachainMainnet } from '@/config/berachain-mainnet'
 
 export const supportedChains = {
   bartio: berachainTestnetbArtio,
   cartio: berachainTestnetcArtio,
+  mainnet: berachainMainnet,
 } as const
