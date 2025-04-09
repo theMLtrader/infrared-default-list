804b11da51e915fb69dd87a3f3a7a563d4292b97
Dorian Wilhelm <wilhelm.dorian@pm.me>
2025-04-09 15:38:10 +0200

    feat: check for duplicate tokens

--- scripts/_/validate-token-details.ts
+++ scripts/_/validate-token-details.ts
@@ -133,7 +133,17 @@ export const validateTokenDetails = async ({
   publicClient: PublicClient
   tokens: TokensSchema['tokens']
 }) => {
+  const addresses = new Set<string>()
+
   for (const token of tokens) {
+    const lowercasedAddress = token.address.toLowerCase()
+    if (addresses.has(lowercasedAddress)) {
+      errors.push(
+        `Error in tokens: Duplicate token address found: ${token.address}`,
+      )
+    }
+    addresses.add(lowercasedAddress)
+
     await validateDecimals({ errors, publicClient, token })
     await validateImage({
       errors,
